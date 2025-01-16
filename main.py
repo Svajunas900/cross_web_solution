b7b742b6fc65390c66702f1a758a684c575cbac9
Svajunas900 <svajunaskoncius11@gmail.com>
2025-01-16 16:05:59 +0200

    Add csv and display it in routes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 
 
 app = Flask(__name__)
@@ -11,9 +12,13 @@ def hello_world():
 
 @app.route("/current_compet_info")
 def current_competitors_info():
-    return
+    df = pd.read_csv('competitors.csv')
+    return render_template("current_info.html", data=df.to_dict())
 
 
-@app.route("/letter_compet/{letter}")
-def letter_competitor():
-    return
+@app.route("/letter_compet/<letter>")
+def letter_competitor(letter):
+    df = pd.read_csv('competitors.csv')
+    df = df.where(df['name'].str.startswith(letter)).dropna()
+    df = pd.DataFrame(df)
+    return render_template("letter_compet.html", data=df.to_dict())
